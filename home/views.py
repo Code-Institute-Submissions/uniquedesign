99899d3a11cc81536d1a7a25819302a4c20493fa
Hidayat Mansuri <hidayat.mansuri@gmail.com>
2020-08-23 18:53:26 +0000

    Added allauth templates, Mobile and Main Nav bar, Changed settings and urls accordingly for allauth

--- home/views.py
+++ home/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def design(request):
-    return render(request, 'design.html')
+def index(request):
+    return render(request, 'index.html')
--- uniquedesign/urls.py
+++ uniquedesign/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from design.views import index
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
+    path('accounts/', include('allauth.urls')),
+    path('design/', include('design.urls')),
+    path('', include('home.urls')),
 ]
