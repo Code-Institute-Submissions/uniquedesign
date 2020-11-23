be6bf4e6a59971e092647a3de4bb83fcf0de40d3
Hidayat Mansuri <hidayat.mansuri@gmail.com>
2020-11-23 23:53:50 +0000

    Added basic payment functionality

--- payment/views.py
+++ payment/views.py
@@ -1,19 +1,43 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-
+from django.conf import settings
 from .forms import OrderForm
+from purchase.contexts import purchase_items
+import stripe
 
 
 def payment(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     purchase = request.session.get('purchase', {})
     if not purchase:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('design'))
 
+    current_purchase = purchase_items(request)
+    total = current_purchase['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'payment.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
