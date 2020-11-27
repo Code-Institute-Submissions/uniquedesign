f4fb35d9c386900761ade7b06afb75c603abc7d6
Hidayat Mansuri <hidayat.mansuri@gmail.com>
2020-11-27 21:40:02 +0000

    Updated Payment form

--- payment/models.py
+++ payment/models.py
@@ -3,9 +3,9 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django_countries.fields import CountryField
 
 from design.models import Product
-
 
 class Order(models.Model):
     order_number = models.CharField(max_length=13, null=False, editable=False)
@@ -16,7 +16,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=False, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
