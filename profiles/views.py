19454284a6de426fd5cccc7272e3d694e4bc3977
Hidayat Mansuri <hidayat.mansuri@gmail.com>
2020-11-28 01:23:30 +0000

    Completed confirmation emails

--- profiles/views.py
+++ profiles/views.py
@@ -44,7 +44,7 @@ def order_history(request, order_number):
         'A confirmation email was sent on the order date.'
     ))
 
-    template = 'checkout/checkout_success.html'
+    template = 'payment_success.html'
     context = {
         'order': order,
         'from_profile': True,
